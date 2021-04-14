1080509c153382af9bd7ddd2ccba15d09b7d6054
AntoineBardy <antoine.bardy11@gmail.com>
2021-04-14 10:55:17 +0200

    Update monstre.h

--- monstre.h
+++ monstre.h
@@ -1,22 +1,26 @@
-#include <string>
-#include <vector>
+#ifndef DEF_MONSTRE
+#define DEF_MONSTRE
+
 #include <iostream>
+#include <string>
+ 
 class Monstre
 {
     public:
-
+ 
     Monstre();
     Monstre(std::string nom, int pv, int degats, bool etat);
-    void Affichage ();
-    void Attaque(Monstre&ennemis);
-    void priseDegats (int nbDegats);
-    int getDegats () const;
-
+    void Affichage();
+    void Attaque(Monstre &ennemis);
+    void priseDegats(int nbDegats);
+    int getDegats() const;
+ 
     private:
-
+ 
     std::string nomM;
     int hp;
     int atq;
     bool currentState;
-
 };
+ 
+#endif
