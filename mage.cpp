cdcc90852a56bcb65b05225fcf4b09ec806d6cb7
AntoineBardy <antoine.bardy11@gmail.com>
2021-04-14 11:40:01 +0200

    Update mage.cpp

---
+++ mage.cpp
@@ -0,0 +1,28 @@
+#ifndef MAGE_CPP
+#define MAGE_CPP
+
+#include "Mage.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+Mage::Mage(): _name("Mage"), _hp(30){
+    Monstre test = Monstre();
+    _board.push_back(test);
+
+}
+
+void Mage::printStat(){
+    std::cout << "Name: " << _name << " || Hp: " << _hp  << std::endl << "Board:" << std::endl;
+    for (int i = 0; i < _board.size(); i++){
+        _board[i].printStat();
+    }
+}
+
+void Mage::ajoutMonstre(Monstre monster){
+    _board.push_back(monster);
+}
+
+
+
+#endif
